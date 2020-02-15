6502989c1308397b1cf4031bd71bb9ede523317f
rexim <reximkut@gmail.com>
2020-02-15 18:50:16 +0700

    Remove TODO(#72)

--- src/KGBotka/Bot.hs
+++ src/KGBotka/Bot.hs
@@ -194,7 +194,6 @@ evalExpr (FunCallExpr "friday" args) = do
 -- TODO(#69): %asciify does not support FFZ emotes
 -- TODO(#70): %asciify does not support BTTV emotes
 -- TODO(#71): %asciify does not have a cooldown
--- TODO(#72): %asciify does not have trusted filter
 evalExpr (FunCallExpr "asciify" args) = do
   failIfNotTrusted
   emotes <- evalContextTwitchEmotes <$> get
