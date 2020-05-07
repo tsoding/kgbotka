e83f1bc1288d214c1d5748223109aba6b320f688
rexim <reximkut@gmail.com>
2020-05-08 01:14:20 +0700

    Add TODO(#179)

--- src/KGBotka/Eval.hs
+++ src/KGBotka/Eval.hs
@@ -411,7 +411,7 @@ evalExpr (FunCallExpr "calc" args) = do
     parserStopToEvalError EOF = EvalError "Calc: Unexpected EOF"
     parserStopToEvalError (SyntaxError msg) =
       EvalError $ "Syntax error: " <> msg
-    -- TODO: There might be a better way to do the job of calcResultToEval
+    -- TODO(#179): There might be a better way to do the job of calcResultToEval
     -- instead of unwrapping the ExceptT
     calcResultToEval :: Either CalcEvalError Double -> Eval T.Text
     calcResultToEval calcResult =
