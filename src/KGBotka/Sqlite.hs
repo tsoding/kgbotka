fe2934780a96a2d731befe9df5ff0b58141be1af
rexim <reximkut@gmail.com>
2020-05-22 02:16:02 +0700

    Add TODO(#208)

--- src/KGBotka/Sqlite.hs
+++ src/KGBotka/Sqlite.hs
@@ -3,7 +3,7 @@ module KGBotka.Sqlite where
 import Control.Concurrent
 import Database.SQLite.Simple
 
--- TODO: make withLockedTransaction to always handle Exceptions from Sqlite library
+-- TODO(#208): make withLockedTransaction to always handle Exceptions from Sqlite library
 --   Give the user some endpoints to log the errors
 withLockedTransaction :: MVar Connection -> (Connection -> IO a) -> IO a
 withLockedTransaction mvarDbConn action =
